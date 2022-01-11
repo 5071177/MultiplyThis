34f3e7cd6b80cc776499d4e179a846df98c1ad5d
Yury Prokhorov <yury.prokhorov@appewa.com>
2022-01-11 20:50:51 +0300

    Initial Commit

---
+++ MultiplyThis/MultiplyThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyThisApp.swift
+//  MultiplyThis
+//
+//  Created by Yury Prokhorov on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
