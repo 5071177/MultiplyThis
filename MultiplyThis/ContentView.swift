940a54eea5ec1059ef2e96d56fdd0a6863ac333f
Yury Prokhorov <yury.prokhorov@appewa.com>
2022-01-11 21:01:59 +0300

    Prework

--- MultiplyThis/ContentView.swift
+++ MultiplyThis/ContentView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstNumber = 0
+    @State private var secondNumber = 0
+    
+    @State private var numberOfQuestions = 1
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView {
+                   Form {
+        Section("How many questions do you want to receive?") {
+                            Picker("\(numberOfQuestions) questions", selection: $numberOfQuestions) {
+                                ForEach(1..<21){
+                                    Text(String($0))
+                                }
+                        }
+        }
+                       
+                   }
+            
+        }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
