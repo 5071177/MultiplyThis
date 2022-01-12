da42948e395390ae38d40e1ce956ea7aeac4f0dd
Yury Prokhorov <yury.prokhorov@appewa.com>
2022-01-12 20:10:21 +0300

    Trying to solve this shit

--- MultiplyThis/ContentView.swift
+++ MultiplyThis/ContentView.swift
@@ -7,25 +7,66 @@
 
 import SwiftUI
 
+
+// TODO: Добавить guard на количество вопросов (больше 0)
+// TODO: Добавить guard на максимальное число (больше 4)
+// TODO: Добавить наполнение групп рандомными числами от 1 до maxNumber
+
+
 struct ContentView: View {
     
-    @State private var firstNumber = 0
-    @State private var secondNumber = 0
+    @State private var maxNumber = 0
+    @State private var numberOfQuestions = 0
+    @State private var listOfQuestions = ["test", "test2"]
     
-    @State private var numberOfQuestions = 1
+    @State private var firstGroup: Array<Int>
+    @State private var secondGroup: Array<Int>
     
+    @State private var playerScore = 0
+    
+    
+    func startGame() {
+        
+        playerScore = 0
+        
+        
+        listOfQuestions.removeAll()
+        
+        
+        for number in 4..<maxNumber{
+            listOfQuestions.append("\(number) * \(number)")
+        }
+        
+        
+        return
+    }
     
     var body: some View {
         NavigationView {
                    Form {
         Section("How many questions do you want to receive?") {
-                            Picker("\(numberOfQuestions) questions", selection: $numberOfQuestions) {
-                                ForEach(1..<21){
-                                    Text(String($0))
-                                }
-                        }
+            Stepper("\(numberOfQuestions) questions", value: $numberOfQuestions, in: 0...20, step: 5)
+
         }
                        
+        Section("Choose the maximum number you want to practice with"){
+            Picker("\(maxNumber)", selection: $maxNumber) {
+                ForEach(4..<13){
+                    Text("\($0)")
+                               }
+                           }
+                       }
+                       Section("Press button to start the game"){
+                           Button("Start game", action: startGame)}
+                       
+                       Section("Answer questions bellow"){
+                       List{
+                           ForEach(listOfQuestions, id: \.self){
+                               Text($0)
+                           }
+                           
+                       }
+                       }
                    }
             
         }
